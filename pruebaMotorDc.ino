b0daa0796a4fb94e41c074e443549a18e5774e0f
Emiliano Castro <93452814+Humol-e@users.noreply.github.com>
2026-02-14 16:38:33 -0600

    Refactor Motor A speed control implementation
    Updated speed control for Motor A by changing PWMA to use analogWrite for variable speed.

--- pruebaMotorDc.ino
+++ pruebaMotorDc.ino
@@ -1,13 +1,15 @@
+//este codigo utiliza la librería esp32 de pablopeza: https://github.com/pablopeza/TB6612FNG_ESP32 , instalalo y  agrega e .zip en sketch :)
 #include <TB6612_ESP32.h>
 
 // Pines para el Motor A
 #define AIN1 23
 #define AIN2 22
-#define PWMA 18  // Tu nuevo pin de velocidad
+#define PWMA 18  // Pin controlador de velocidad
+
 #define STBY 4
 
 // Creamos solo el objeto del Motor 1
-// Usamos los 8 parámetros que ya sabemos que tu librería acepta
+// Usamos los 8 parámetros
 Motor motor1 = Motor(AIN1, AIN2, 0, 1, STBY, 5000, 8, 0);
 
 void setup() {
@@ -16,7 +18,7 @@ void setup() {
   // Configuración manual del pin de velocidad (PWMA)
   // Como lo conectaste al 18, necesitamos decirle al ESP32 que mande energía por ahí
   pinMode(PWMA, OUTPUT);
-  digitalWrite(PWMA, HIGH); // Esto le da "gasolina" constante al motor
+  analogWrite(PWMA, 50);  // Este valor de 0-255 dictamina la velocidad maxima del motor
 
   Serial.println("Probando SOLO Motor A...");
   Serial.println("PWMA conectado al pin 18");
@@ -25,11 +27,11 @@ void setup() {
 
 void loop() {
   Serial.println("Motor A adelante...");
-  motor1.drive(150); // Velocidad media
+  motor1.drive(150);  // Velocidad media
   delay(2000);
 
   Serial.println("Motor A atrás...");
-  motor1.drive(-150); // Reversa
+  motor1.drive(-150);  // Reversa
   delay(2000);
 
   Serial.println("Frenando...");
